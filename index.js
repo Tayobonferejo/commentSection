beba6e4fe85ace683adeb5eac892f50ce9d18f86
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-10-15 23:28:34 +0100

    more js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 async function loadComment() {
+
     try {
+            // Step 1: Check localStorage first
         let data = JSON.parse(localStorage.getItem("comment"));
 
         if (!data) {
@@ -9,8 +11,33 @@ async function loadComment() {
             }
             data = await response.json();
             console.log(data);
+            localStorage.setItem("comment", JSON.stringify(data));
         }
-    }
+
+        const commentContainer = document.getElementById("commentContainer");
+
+        // Step 4: Render each card
+        filteredData.forEach((element, index) => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+        <div class="card-container">
+            <div class="card-header">
+                <img src="${element.logo}" alt="${element.name}" class="icon">
+            </div>
+            <div class="desCont">
+                <div class="name">${element.name}</div>
+                <div class="description">${element.description}</div>
+            </div>
+        </div>
+        <div class="control">
+            <button class="remove-btn">Remove</button>
+            <div class="toggle ${element.isActive ? "active" : ""}"></div>
+        </div>
+        `;
+    });
+}
 
     catch (error) {
         console.error("Error fetching data:",(error));
